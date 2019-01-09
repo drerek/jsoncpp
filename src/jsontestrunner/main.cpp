a37c6f4c9a8ecfadbd2edef796eae1b8b6db8d60
Artem Chernousov <adrerek@gmail.com>
2019-01-09 14:15:25 +0200

    patch

--- src/jsontestrunner/main.cpp
+++ src/jsontestrunner/main.cpp
@@ -64,8 +64,10 @@ static JSONCPP_STRING readInputTestFile(const char* path) {
   if (fread(buffer, 1, usize, file) == usize)
     text = buffer;
  delete[] buffer;
-  if (file != NULL)
+  if (file != NULL){
     fclose(file);
+file = NULL;
+}
  return text;
 }
 
